5f9fb41d00398b355d15340917b06e048fc84146
Zheng Yang Tean <teanzhengyang@outlook.sg>
2017-10-31 16:50:12 +0800

    Elastictopology

---
+++ src/Demo/ElasticBoltDemo/ElasticExclaimationBolt.java
@@ -0,0 +1,60 @@
+package Demo.ElasticBoltDemo;
+
+import com.twitter.heron.api.bolt.BaseElasticBolt;
+import com.twitter.heron.api.bolt.OutputCollector;
+import com.twitter.heron.api.metric.GlobalMetrics;
+import com.twitter.heron.api.topology.OutputFieldsDeclarer;
+import com.twitter.heron.api.topology.TopologyContext;
+import com.twitter.heron.api.tuple.Fields;
+import com.twitter.heron.api.tuple.Tuple;
+import com.twitter.heron.api.tuple.Values;
+import com.twitter.heron.api.utils.Utils;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by zhengyang on 31/10/17.
+ */
+public class ElasticExclaimationBolt extends BaseElasticBolt {
+    private static final long serialVersionUID = 1711761917521075103L;
+    private OutputCollector collector;
+    private long startTime;
+    private boolean emit;
+    private AtomicInteger nItems;
+
+    public ElasticExclaimationBolt(boolean emit) {
+        this.emit = emit;
+    }
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public void prepare(
+            Map conf,
+            TopologyContext context,
+            OutputCollector acollector) {
+        collector = acollector;
+        nItems = new AtomicInteger(0);
+        startTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        Utils.sleep(10);
+        nItems.getAndIncrement();
+        long latency = System.currentTimeMillis() - startTime;
+        GlobalMetrics.incr("selected_items");
+        System.out.println(tuple.getString(0) + " :: " +nItems.get() + " in " + latency
+                + " num/max: " + getNumCore() + "/" + getMaxCore() + "/" + getNumOutStanding());
+        if (emit) {
+            emitTuple(tuple, new Values(tuple.getString(0) + "!!!"));
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        if (emit) {
+            declarer.declare(new Fields("word"));
+        }
+    }
+}
--- src/Demo/ElasticBoltDemo/ElasticTopology.java
+++ src/Demo/ElasticBoltDemo/ElasticTopology.java
@@ -1,7 +1,47 @@
 package Demo.ElasticBoltDemo;
 
+import Demo.FinalBolt;
+import Demo.VaryingNumKeySprout;
+import com.twitter.heron.api.Config;
+import com.twitter.heron.api.HeronSubmitter;
+import com.twitter.heron.api.topology.TopologyBuilder;
+import com.twitter.heron.api.tuple.Fields;
+
 /**
  * Created by zhengyang on 27/10/17.
  */
 public class ElasticTopology {
+    private ElasticTopology() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new RuntimeException("Please specify the name of the topology");
+        }
+
+        int parallelism = 1;
+        int numThreads = 4;
+
+
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("word", new VaryingNumKeySprout(), 2);
+        // (name, elastic bolt instance, number of cores used, debug, sleepDuration, batches to aggregate)
+        builder.setBolt("exclaim1", new ElasticExclaimationBolt(true), parallelism, numThreads, true, 1)
+                .fieldsGrouping("word", new Fields("word"));
+        builder.setBolt("final", new FinalBolt(false), 1)
+                .fieldsGrouping("exclaim1", new Fields("word"));
+
+        Config conf = new Config();
+        conf.setDebug(true);
+
+
+        // Put an arbitrary large number here if you don't want to slow the topology down
+        conf.setMaxSpoutPending(1000 * 1000 * 1000);
+        conf.put(Config.TOPOLOGY_WORKER_CHILDOPTS, "-XX:+HeapDumpOnOutOfMemoryError");
+
+        // Set number of containers
+        conf.setNumStmgrs(1);
+
+        HeronSubmitter.submitTopology(args[0], conf, builder.createTopology());
+    }
 }
